ef3eba69f5fcbcf0298d0bf2984489324ed41116
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 05:57:02 -0800

    Add files via upload

---
+++ lab13q1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number,reversed_number=0,sumOfDigits=0;
+    cout << "enter number" << endl;
+    cin>>number;
+    while(number>0){
+        int digit=number%10;
+    }
+    cout<<"reverse number:"<< reversed_number<<endl;
+    return 0;
+}
