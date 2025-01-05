6c3e96c399ab7d16de87c1deb7f1675a30593d0e
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 06:05:45 -0800

    Add files via upload

---
+++ lab14.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice[];
+    int seatsAvailable[] = [10];
+    string programs_available[]=["cs","ai","se"];
+    string student_Names[]=["ali","ahmad","hamza"];
+    for(i=0,i<=3,i++)
+        string student_Names[]={"ali","ahmad","hamza};
+        int student_marks[]={80,50,80};
+        cout<<"student_Names"<<"programmes_available"<<"setavailable"<<endl;
+    }
+    return 0;
+}
