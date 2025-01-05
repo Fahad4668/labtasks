8fb495530b1a163dc8f9d57a865c844e59ea6043
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 06:49:01 -0800

    Add files via upload

---
+++ lab13q3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int randomNumber;
+    cout<< "enter randomNumber"<<endl;
+    cin>>randomNumber;
+    for(guess != number){
+        cout<<"inside while loop"<<endl; "
+    }
+    
+    return 0;
+}
