ce20765386fda585a7440873768627ae96bc6d66
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 05:54:25 -0800

    Add files via upload

---
+++ question3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float usd,pkr;
+    cout << "enter value in usd" << endl;
+    cin>>usd;
+    pkr = usd * 75;
+    cout << "value in pkr is: " << pkr << endl;
+    return 0;
+}
