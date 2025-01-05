2b044bd40fea156cf45f0147923aa1da36ec8d99
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 06:00:34 -0800

    Add files via upload

---
+++ lab2task1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << "The number " << num << " is even." << endl;
+    } else {
+        cout << "The number " << num << " is odd." << endl;
+    }
+
+    return 0;
+}
