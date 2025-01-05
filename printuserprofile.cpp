2ca97f9cba3204b3a27c8aa498c4649c38e32855
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 06:52:55 -0800

    Add files via upload

---
+++ printuserprofile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int printuserprofile () {
+    string name="Fahad";
+    int age=18;
+    string sap ="70174668";
+    cout<<"my name is "<<name<<endl;
+    cout<<"my age is " <<age<<endl;
+    cout<<"sap id is " <<sap<<endl;
+    return 0;
+}
+int main() {
+    printuserprofile();
+    printuserprofile();
+    printuserprofile();
+    return 0;
+}
