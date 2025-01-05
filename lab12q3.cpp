cedd5c7221bb1fc75fd18abf7ba84cb6f75566b0
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 06:40:41 -0800

    Add files via upload

---
+++ lab12q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter number";
+    cin>>number;
+    if(number==1,3,5,7,9){
+        cout<<"number is odd";
+    }  else {
+        cout<<"number is even";
+    }
+    return 0;
+}
