f245c1d5dbb0e06f9b454151cedb83dd4118becd
Fahad ALI <fahadalifahadali915@gmail.com>
2025-01-05 07:33:06 -0800

    Add files via upload

---
+++ lab10Q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string percentage =80%;
+    string percentage =90%;
+    cout << "Percentage 1: " << percentage << endl;
+    cout << "Percentage 2: " << percentage << endl;
+    cin>>percentage 1;
+    cin>>percentage 2;
+    float  income 1=50000;
+    float income 2=100000;
+    cout << "income: " << income << endl;
+    cin>>income;
+    string participation in extracurricular activities;
+    cout<<"participation yes or no"<<endl;
+    cin>>participation in extracurricular activities;
+    if(income >50000){
+        cout<<"full scholarship"endl;
+        cin>>"percentage 2";
+    }
+    return 0;
+}
